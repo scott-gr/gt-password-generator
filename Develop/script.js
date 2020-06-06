a3e4370cbed0921a765fa1e9425d066f79892078
Scott Griffin <mscottgriffin@gmail.com>
2020-06-06 15:21:19 -0400

    set character vars as const. Testing function in console log.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,32 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const specialCharacters = '~`!@#$%^&*-_+=<>?/;:[]{}\|';
 
-var randomNumber = Math.floor(Math.random() * 10);
+//number generator
+function getCharacter() {
+  var randomNumber = Math.floor(Math.random() * 10);
+  var randomUpper = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  var randomLower = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+
+  console.log(randomLower);
+  console.log(randomUpper);
   console.log(randomNumber);
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(getCharacter)
+
+
+
+    // // // Write password to the #password input
+    // function writePassword() {
+    //   var password = generatePassword();
+    //   var passwordText = document.querySelector("#password");
+
+    //   passwordText.value = password;
+
+    
+
+    // // Add event listener to generate button
+    // generateBtn.addEventListener("click", writePassword
