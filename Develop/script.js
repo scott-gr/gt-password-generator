2ea08ad35907d36f11693321ecf140952585a6cf
Scott Griffin <mscottgriffin@gmail.com>
2020-06-06 13:43:41 -0400

    created randomNumber var

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var randomNumber = Math.floor(Math.random() * 10);
+  console.log(randomNumber);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
