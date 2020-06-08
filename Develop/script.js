5a453e0e6051d298f0ecfd97e160ad09f5bd583c
Scott Griffin <mscottgriffin@gmail.com>
2020-06-08 13:46:46 -0400

    added confirm pop-ups for each character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // generate button references #generate id in html.index
-const generateBtn = document.querySelector("#generate");
+// const generateBtn = document.querySelector("#generate");
+// generateBtn.addEventListener("click", charOptions);
 // function result will fill in #password id in html.index
 const passwordResult = document.querySelector("#password");
 //Establish possible characters options for letters and special characters
@@ -9,34 +10,61 @@ const specialCharacters = '~`!@#$%^&*-_+=<>?/;:[]{}\|';
 
 
 //Establishes Array of character options, and removes options that the user declines.
-function charOptions() {
+document.getElementById('generate').onclick = function charOptions() {
+  //Chooses random character from variables above
+  const randomNumber = Math.floor(Math.random() * 10); //randomNumber doesn't need to reference a variable, it's choosing an interger 0-9 right here.
+  const randomUpper = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  const randomLower = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+  const randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  //creates array of random character of each type
+  var randomArray = [randomUpper, randomLower, randomNumber, randomSpecial]
+  //confirm variables work, and appear same in array
+  console.log(randomUpper);
+  console.log(randomLower);
+  console.log(randomNumber);
+  console.log(randomSpecial);
+  console.log(randomArray)
+  //randomly chooses one element from randomArray, with all options present
+  // var charChoice = randomArray[Math.floor(Math.random() * randomArray.length)];
+
   ///Starts at 0 until user declares length
+  var passwordLength = 0
   var passwordLength = prompt('Enter a number between 8 and 128 for your desired password length, then click "OK".');
   if (passwordLength < 8 || passwordLength > 128) {
     alert('Please enter a number between 8 and 128 and try again.');
     charOptions();
   }
-}
+  else {
+    useUpper = confirm('Click "OK" to include UPPERCASE letters in your password.');
+    useLower = confirm('Click "OK" to include lowercase letters in your password.');
+    useNumber = confirm('Click "OK" to include numbers in your password.');
+    useSpecial = confirm('Click "OK" to include special characters in your password.');
+    if (useUpper !== true && useLower !== true && useNumber !== true && useSpecial !== true) {
+      alert('You must select at least 1 character type. Please try again.')
+      charOptions();
+    }
 
 
-//Chooses random character from variables above
-const randomNumber = Math.floor(Math.random() * 10); //randomNumber doesn't need to reference a variable, it's choosing an interger 0-9 right here.
-const randomUpper = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
-const randomLower = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
-const randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-//creates array of random character of each type
-var randomArray = [randomLower, randomUpper, randomNumber, randomSpecial]
-//confirm variables work, and appear same in array
-console.log(randomLower);
-console.log(randomUpper);
-console.log(randomNumber);
-console.log(randomSpecial);
-console.log(randomArray)
-//randomly chooses one element from randomArray, with all options present
-var charChoice = randomArray[Math.floor(Math.random() * randomArray.length)];
-console.log(charChoice);
 
-console.log(charOptions());
+
+
+// //Chooses random character from variables above
+// const randomNumber = Math.floor(Math.random() * 10); //randomNumber doesn't need to reference a variable, it's choosing an interger 0-9 right here.
+// const randomUpper = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+// const randomLower = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+// const randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+// //creates array of random character of each type
+// var randomArray = [randomUpper, randomLower, randomNumber, randomSpecial]
+// //confirm variables work, and appear same in array
+// console.log(randomUpper);
+// console.log(randomLower);
+// console.log(randomNumber);
+// console.log(randomSpecial);
+// console.log(randomArray)
+// //randomly chooses one element from randomArray, with all options present
+// var charChoice = randomArray[Math.floor(Math.random() * randomArray.length)];
+// console.log(charChoice);
+
 
 
 
@@ -52,4 +80,3 @@ console.log(charOptions());
 
     // Add event listener to generate button
 
-generateBtn.addEventListener("click", charOptions);
